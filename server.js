b74449703e620f470724dd9d7985a54543904e00
Preston Young <prestonyoung@Prestons-MBP.attlocal.net>
2020-08-27 19:13:46 -0700

    Connected back end server to database for profile route

--- server.js
+++ server.js
@@ -78,15 +78,19 @@ app.post('/register', (req, res) => {
 app.get('/profile/:id', (req, res) => {
     const {id} = req.params;
     let found = false;
-    database.users.forEach(user => {
-        if (user.id === id){
-            found = true;
-            return res.json(user);
+    db.select('*').from('users')
+    .where({id})
+    .then(user => {
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(404).json("no such user");
         }
-    });
-    if (!found) {
-        res.status(404).json("no such user");
-    }
+    })
+    .catch(err => res.status(404).json("error getting user"));
+    // if (!found) {
+    //     res.status(404).json("no such user");
+    // }
 });
 
 // Image route
