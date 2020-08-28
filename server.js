06522e8b445aba57667b8aae5f065abf60a2f90b
Preston Young <prestonyoung@Prestons-MBP.attlocal.net>
2020-08-27 20:25:44 -0700

    Connected back end server to database for signin route

--- server.js
+++ server.js
@@ -46,28 +46,59 @@ app.get('/', (req, res) => {
 
 // Sign in route
 app.post('/signin', (req, res) => {
-    if (req.body.email === database.users[0].email &&
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]);
+    db.select('email', 'hash').from('login')
+    .where({
+        'email': req.body.email
+    })
+    .then(data => {
+        if (data.length) {
+            if (bcrypt.compareSync(req.body.password, data[0].hash)) {
+                return db.select('*').from('users')
+                .where({
+                    'email': req.body.email
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+                .catch(err => res.status(400).json('unable to get user'))
+            } else {
+                res.status(400).json('password and username do not match');
+            }
         } else {
-            res.status(404).json("Error logging in");
+            res.status(400).json('password and username do not match');
         }
+    })
+    .catch(err => res.status(400).json('unable to login'))
 });
 
 // Register route
 app.post('/register', (req, res) => {
     const { email, password, name } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .then(user => {
-            res.json(user[0]);
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0],
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
         })
-        .catch(err => res.status(404).json('Already registered under this email'));
+        .then(trx.commit)
+        .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('Unable to register'));
+    
 });
 
 // Profile route
